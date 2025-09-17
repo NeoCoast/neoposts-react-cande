8be0b63ea46f1766f5b207f5e3156d285762539e
Candelaria <candelaria.lopez@neocoast.com>
2025-09-17 10:57:25 -0300

    feat(lint): add linters

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.scss'
-import App from './App.jsx'
-import './style.scss'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.scss';
+import App from './App.jsx';
+import './style.scss';
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.querySelector('#root')).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
